4cf3e674042c33ae6c274779b5f21f04fdceaebc
007ash <6ashwanth007@gmail.com>
2025-12-18 20:37:53 +0530

    API DONE

--- Znew work/app.py
+++ Znew work/app.py
@@ -8,23 +8,26 @@ model = joblib.load('model/greenroute_model.pkl')
 
 @app.post("/predict")
 def predict_co2(data: dict):
-    vechile_weight_map = {'car': 1500, 'bike': 150}
+    vehicle_weight_map = {'car': 1500, 'bike': 150}
     traffic_impact_map = {'low':1.0, 'medium':1.4, 'high':2.5}
-    congestion_factor = traffic_impact_map[data["traffic"]] * data["distance"]
-    effective_speed = data["avg_speed"] / traffic_impact_map[data["traffic"]]
-    stress_score = vechile_weight_map[data["vehicle"]] * congestion_factor
+    vehicle_weight = vehicle_weight_map.get(data['vehicle'])
+    traffic_impact = traffic_impact_map.get(data['traffic'])
+    data['vehicle'] = vehicle_weight 
+    data['traffic'] = traffic_impact
+    data['congestion_factor'] = data['traffic']*data['distance']
+    data['effective_speed'] = data['avg_speed'] / data['traffic']
+    data['stress_score'] = data['vehicle'] * data['congestion_factor'] 
+
     features = np.array([[
         data["distance"],
         data["avg_speed"],
-        vechile_weight_map[data["vehicle"]],
-        traffic_impact_map[data["traffic"]],
-        congestion_factor,
-        effective_speed,
-        stress_score
+        data["vehicle"],
+        data["traffic"],
+        data["congestion_factor"],
+        data["effective_speed"],
+        data["stress_score"]
     ]])
 
     co2 = model.predict(features)[0]
 
-    return {
-        "predicted_co2_emission": round(float(co2), 2)
-    }
+    return {"co2_emission": round(float(co2), 2)}
