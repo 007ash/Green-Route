e7c45d9791bb57e2f383152e62c7201737543726
007ash <6ashwanth007@gmail.com>
2025-12-18 20:07:05 +0530

    model

--- Znew work/app.py
+++ Znew work/app.py
@@ -8,11 +8,19 @@ model = joblib.load('model/greenroute_model.pkl')
 
 @app.post("/predict")
 def predict_co2(data: dict):
+    vechile_weight_map = {'car': 1500, 'bike': 150}
+    traffic_impact_map = {'low':1.0, 'medium':1.4, 'high':2.5}
+    congestion_factor = traffic_impact_map[data["traffic"]] * data["distance"]
+    effective_speed = data["avg_speed"] / traffic_impact_map[data["traffic"]]
+    stress_score = vechile_weight_map[data["vehicle"]] * congestion_factor
     features = np.array([[
-        data["distance_km"],
-        data["avg_speed_kmph"],
-        data["vehicle_type"],
-        data["traffic_level"]
+        data["distance"],
+        data["avg_speed"],
+        vechile_weight_map[data["vehicle"]],
+        traffic_impact_map[data["traffic"]],
+        congestion_factor,
+        effective_speed,
+        stress_score
     ]])
 
     co2 = model.predict(features)[0]
