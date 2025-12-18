61f51d482cc45d4837f0f42f3a6149a8e6ac47cf
007ash <6ashwanth007@gmail.com>
2025-12-18 19:49:29 +0530

    API

--- Znew work/appAPI.py
+++ Znew work/appAPI.py
@@ -3,3 +3,20 @@ import joblib
 import numpy as np
 
 app = FastAPI()
+
+model = joblib.load('model/greenroute_model.pkl')
+
+@app.post("/predict")
+def predict_co2(data: dict):
+    features = np.array([[
+        data["distance_km"],
+        data["avg_speed_kmph"],
+        data["vehicle_type"],
+        data["traffic_level"]
+    ]])
+
+    co2 = model.predict(features)[0]
+
+    return {
+        "predicted_co2_emission": round(float(co2), 2)
+    }
